7dcc6b4b300638cf614fb9614e7254f5d00a8d5f
Nadjakoroleva <nadin.koroleva@gmail.com>
2018-07-26 20:18:55 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function setup() {
+	createCanvas(500, 500);
+}
+  
+function draw() {
+	fill(5, 255, 65);
+  ellipse(mouseX, mouseY, 300, 300);
+  fill(5, 111, 255);
+  ellipse(mouseX, mouseX, 60, 60);
+  fill(10, 174, 247);
+  ellipse(300, 200, 50, 50);
+  fill(163, 10, 247);
+  rect(190, 300, 130, 10);
+}
+
